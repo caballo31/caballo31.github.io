a781f1cd05cfcd26ff075733e874b5d9d9b6d934
caballo31 <versallialejandro@gmail.com>
2023-01-25 15:58:51 -0300

    Test only vibration

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const progressBar = document.querySelector('.progress-bar');
 
 
 function start(){
+    navigator.vibrate(70);
     progressBar.setAttribute('id','play-animation');
     let counter = 0;
     function vibration () {
         counter++;
         if (counter == 5) {
-            window.navigator.vibrate(180);
+            navigator.vibrate(180);
             counter = 0;
         } else {
-            window.navigator.vibrate(70);
+            navigator.vibrate(70);
         }
     }
     setInterval(vibration, 1000)
