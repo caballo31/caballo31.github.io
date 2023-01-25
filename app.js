34f75bd05a4259960906b769e3f34c83416afbcd
caballo31 <versallialejandro@gmail.com>
2023-01-25 15:40:26 -0300

    Add vibration funtion to test in phone

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 
 const progressBar = document.querySelector('.progress-bar');
 
+
+
 function start(){
-    progressBar.setAttribute('id','play-animation')
+    progressBar.setAttribute('id','play-animation');
+    let counter = 0;
+    function vibration () {
+        counter++;
+        if (counter == 5) {
+            window.vibrate(180);
+            counter = 0;
+        } else {
+            window.vibrate(70);
+        }
+    }
+    setInterval(vibration, 1000)
 }
