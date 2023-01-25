639dfa7e210e967425d8f5183ad71c8ee41a78bb
caballo31 <versallialejandro@gmail.com>
2023-01-25 16:17:12 -0300

    Add stop funtion and logo

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 
 const progressBar = document.querySelector('.progress-bar');
 
-
+let codeStopped = true;
 
 function start(){
-    progressBar.setAttribute('id','play-animation');
-    let counter = 0;
-    function vibration () {
-        counter++;
-        if (counter == 5) {
-            window.navigator.vibrate(180);
-            counter = 0;
-        } else {
-            window.navigator.vibrate(70);
+    if (codeStopped == true){
+        progressBar.setAttribute('id','play-animation');
+        let counter = 0;
+        function vibration () {
+            counter++;
+            if (counter == 5) {
+                window.navigator.vibrate(180);
+                counter = 0;
+            } else {
+                window.navigator.vibrate(70);
+            }
         }
+        setInterval(vibration, 1000)
+        codeStopped = false;
+    } else if (codeStopped == false) {
+        progressBar.setAttribute('id', '');
+        clearInterval();
+        codeStopped = true;
     }
-    setInterval(vibration, 1000)
 }
